67e13f08c8f60627205adea5068084796c754232
InsafNilam <insafnilam.2000@gmail.com>
2023-10-02 13:04:51 +0530

    Fix MongoDB Connection String Protection

--- Backend/src/Config/DB.js
+++ Backend/src/Config/DB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://DevX:DevX1234@cluster0.tg3da.mongodb.net/hms', {
+        const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.tg3da.mongodb.net/${process.env.DB_NAME}`, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     });
